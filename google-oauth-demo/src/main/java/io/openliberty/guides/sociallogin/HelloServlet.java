649f45ebd1b45b947c65d794203116cbccc37cf8
lgad31vn <lgad31vn@gmail.com>
2022-02-24 02:58:53 -0500

    added logout, more on request and session

--- google-oauth-demo/src/main/java/io/openliberty/guides/sociallogin/HelloServlet.java
+++ google-oauth-demo/src/main/java/io/openliberty/guides/sociallogin/HelloServlet.java
@@ -22,13 +22,19 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
+import com.ibm.websphere.security.jwt.JwtToken;
+import com.ibm.websphere.security.social.UserProfileManager;
+
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.security.Principal;
+import java.util.HashMap;
+import java.util.Map;
 
 @WebServlet(name = "HelloServlet", urlPatterns = "/hello")
 @ServletSecurity(value = @HttpConstraint(rolesAllowed = {"students", "professor"},
         transportGuarantee = ServletSecurity.TransportGuarantee.CONFIDENTIAL))
-// @DeclareRoles({"professor", "student"})
+@DeclareRoles("techA")
 public class HelloServlet extends HttpServlet {
 
     private static final long serialVersionUID = 1L;
@@ -41,29 +47,58 @@ public class HelloServlet extends HttpServlet {
         HttpSession session = request.getSession();           
         
         String username = request.getUserPrincipal().getName();
-        request.setAttribute("username", username);
+        session.setAttribute("userRole", "admin");
 
+        String role = (String) session.getAttribute("userRole");
+        String authType = request.getAuthType();
         
         String name = request.getRemoteUser();
-        String role = request.getParameter("security-role");
+        // String role = request.getParameter("security-role");
 
         Boolean res = request.isUserInRole("students");
-                
+        
+        Principal p =  request.getUserPrincipal();
+
+        String accessToken = UserProfileManager.getUserProfile().getAccessToken();
+        String encryptedAccessToken = UserProfileManager.getUserProfile().getEncryptedAccessToken();
+        long tokenLifeTime = UserProfileManager.getUserProfile().getAccessTokenLifeTime();
+        JwtToken jwtToken = UserProfileManager.getUserProfile().getIdToken();
+        String refreshToken = UserProfileManager.getUserProfile().getRefreshToken();
+
+        session.setAttribute("accessToken", accessToken);
+
+        Map<String, String> requestBody = new HashMap<>();
+        requestBody.put("access_token", accessToken);
+
+
+        
         pwriter.println("user name is: " +username+ " ------------- ");
-        pwriter.println("user role is: " +role);
+        pwriter.println("user role is: " +role+ "-----------------");
+        pwriter.println("isUserInRole('admin'): " +res+ "------------");
+        pwriter.println("authType is: " +authType+ "----------------");
+        pwriter.println("accessToken is: " +requestBody+ "----------------");
+        pwriter.println("encryptedAccessToken is: " +encryptedAccessToken+ "----------------");
+        pwriter.println("tokenLifeTime is: " +tokenLifeTime+ "----------------");
+        pwriter.println("jwtToken is: " +jwtToken+ "----------------");
+        pwriter.println("refreshToken is: " +refreshToken+ "----------------");
 
-
-
-
-
-
-
+        
+        
         pwriter.close();
+
+
+
+
+
         // response.setContentType("text/plain");
         // response.setCharacterEncoding("UTF-8");
 
-        // requestuest
-        //         .getrequestuestDispatcher("securedHello.jsp")
-        //         .forward(requestuest,response);
+        // String username = request.getUserPrincipal().getName();
+        // request.setAttribute("username", username);
+
+        // request
+        //         .getRequestDispatcher("securedHello.jsp")
+        //         .forward(request,response);
+    
     }
 }
--- google-oauth-demo/src/main/java/io/openliberty/guides/sociallogin/logout/GitHubLogout.java
+++ google-oauth-demo/src/main/java/io/openliberty/guides/sociallogin/logout/GitHubLogout.java
@@ -12,13 +12,53 @@
 // end::copyright[]
 package io.openliberty.guides.sociallogin.logout;
 
+import com.ibm.websphere.security.social.UserProfileManager;
+import org.eclipse.microprofile.config.inject.ConfigProperty;
+
 import javax.enterprise.context.RequestScoped;
+import javax.inject.Inject;
+import javax.ws.rs.client.ClientBuilder;
+import javax.ws.rs.client.Entity;
 import javax.ws.rs.core.Response;
+import java.nio.charset.StandardCharsets;
+import java.util.Base64;
+import java.util.HashMap;
+import java.util.Map;
 
 @RequestScoped
 public class GitHubLogout implements ILogout {
 
+    // @Inject
+    // @ConfigProperty(name="github.client.id")
+    private String clientId = "06fd8e0f745e0d2112c8";
+
+    // @Inject
+    // @ConfigProperty(name="github.client.secret")
+    private String clientSecret = "645a9c8f52a0b9a086776674e1805db0296f4134";
+
     public Response logout() {
-        return null;
+
+        final String unauthorizeUrl = "https://api.github.com/applications/645a9c8f52a0b9a086776674e1805db0296f4134/grant";
+
+        String accessToken = UserProfileManager
+                .getUserProfile()
+                .getAccessToken();
+
+        Map<String, String> requestBody = new HashMap<>();
+        requestBody.put("access_token", accessToken);
+
+        String auth = clientId + ":" + clientSecret;
+        byte[] encodedAuthStream = Base64
+                .getEncoder()
+                .encode(auth.getBytes(StandardCharsets.ISO_8859_1));
+        String encodedAuth = new String(encodedAuthStream);
+
+        return ClientBuilder
+                .newClient()
+                .target(unauthorizeUrl)
+                .resolveTemplate("client_id", clientId)
+                .request()
+                .header("Authorization", "Basic " + encodedAuth)
+                .method("DELETE", Entity.json(requestBody));
     }
 }
